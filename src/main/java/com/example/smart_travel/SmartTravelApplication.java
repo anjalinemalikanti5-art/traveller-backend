ddf1d1bb47ce4200dacde60ef0d861f047c019f8
anjalinemalikanti5-art <anjalinemalikanti5@gmail.com>
2026-04-01 14:50:27 +0530

    fixed code

--- src/main/java/com/example/smart_travel/SmartTravelApplication.java
+++ src/main/java/com/example/smart_travel/SmartTravelApplication.java
@@ -2,25 +2,11 @@ package com.example.smart_travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmartTravelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartTravelApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:3001"); // React frontend port
-            }
-        };
     }
 }
